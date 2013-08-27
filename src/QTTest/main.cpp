ff704c3c65474357812416799398879f95c67f4e
caostorm <caostorm@894f9759-fa2a-45cf-9159-0a04a166245e>
2013-08-27 02:41:29 +0000

    添加jpeg图片支持
    git-svn-id: svn://192.168.2.188/qtcms@27 894f9759-fa2a-45cf-9159-0a04a166245e

--- src/QTTest/main.cpp
+++ src/QTTest/main.cpp
@@ -5,6 +5,12 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+	// Get Application Path
+	QString sTemp = QCoreApplication::applicationDirPath();
+
+	// extern libraries path
+	QString sExternLib(sTemp + "/exlibs");
+	QApplication::addLibraryPath(sExternLib);
 
     QJaWebView view;
     view.showMaximized();
