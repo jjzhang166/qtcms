6d175d8401069f7b58b70d3d7a9dedeeb406fd4e
caostorm <caostorm@894f9759-fa2a-45cf-9159-0a04a166245e>
2013-09-17 05:56:40 +0000

    调整输出目录结构，界面文件统一放在skins里 通过脚本updateoutput可将界面文件更新到输出的相应目录下
    git-svn-id: svn://192.168.2.188/qtcms@49 894f9759-fa2a-45cf-9159-0a04a166245e

---
+++ updateoutput.bat
@@ -0,0 +1,30 @@
+@echo off
+set /p updateDir="witch one do you wanto update? 1 for debug; 2 for release; 3 for all."
+
+if %updateDir% == 1 (
+set desDir=.\output\Debug\
+goto :copyfile
+)
+if %updateDir% == 2 (
+set desDir=.\output\Release\
+goto :copyfile
+)
+if %updateDir% == 3 (goto :all) else (goto :errorend)
+
+:copyfile
+xcopy web\*.* %desDir%\skins\default\ /Y/S/E
+goto :end
+
+
+:all
+xcopy web\*.* .\output\Debug\skins\default\ /Y/S/E
+xcopy web\*.* .\output\Release\skins\default\ /Y/S/E
+
+:end
+pause
+exit
+
+:errorend
+echo Input error
+pause
+exit
