37133b195e0afd4428fb03fc8dd094ee9c7c0ec4
YangMH <YangMH@894f9759-fa2a-45cf-9159-0a04a166245e>
2013-11-08 02:43:49 +0000

    添加样式配置的代码
    git-svn-id: svn://192.168.2.188/qtcms@157 894f9759-fa2a-45cf-9159-0a04a166245e

--- src/plugins/PreviewWindows/qsubview.cpp
+++ src/plugins/PreviewWindows/qsubview.cpp
@@ -1,6 +1,9 @@
 #include "qsubview.h"
 #include <QtGui/QPainter>
 
+#include <QtCore>
+#include <QSettings>
+
 QSubView::QSubView(QWidget *parent)
 	: QWidget(parent)
 {
@@ -15,10 +18,47 @@ QSubView::~QSubView()
 void QSubView::paintEvent( QPaintEvent * e)
 {
 	QPainter p(this);
-	QRect rcClient = contentsRect();
-	p.fillRect(rcClient,QColor(0,0,0));
-	p.setPen(QColor(255,0,0));
-	p.drawRect(rcClient);
+
+	QString image;
+	QColor LineColor;
+	QColor FontColor;
+	int FontSize;
+	QString FontFamily;
+
+	QString sAppPath = QCoreApplication::applicationDirPath();
+	QString path = sAppPath + "/skins/default/css/SubWindowStyle.ini";
+	QSettings IniFile(path, QSettings::IniFormat, 0);
+
+	image = IniFile.value("background/background-image", NULL).toString();
+	LineColor.setNamedColor(IniFile.value("background/background-color", NULL).toString());
+	FontColor.setNamedColor(IniFile.value("font/font-color", NULL).toString());
+	FontSize = IniFile.value("font/font-size", NULL).toString().toInt();
+	FontFamily = IniFile.value("font/font-family", NULL).toString();
+
+ 	QRect rcClient = contentsRect();
+ 
+ 	QPixmap pix;
+	QString PixPaht = sAppPath + image;
+ 	bool ret = pix.load(PixPaht);
+ 
+  	pix = pix.scaled(rcClient.width(),rcClient.height(),Qt::KeepAspectRatio);
+
+ 	p.drawPixmap(rcClient,pix);
+
+	QPen pen = QPen(LineColor, 2);
+ 	p.setPen(pen);
+ 	p.drawRect(rcClient);
+
+	QFont font(FontFamily, FontSize, QFont::Bold);
+	
+	p.setFont(font);
+
+ 	pen.setColor(FontColor);
+	
+ 	p.setPen(pen);
+
+	p.drawText(rcClient, Qt::AlignCenter, "Pixmap");
+
 }
 
 void QSubView::mouseDoubleClickEvent( QMouseEvent * ev)
