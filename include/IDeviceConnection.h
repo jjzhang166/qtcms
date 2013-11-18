37738312a243a1dcc171a71b5ef68574ca109ee5
CaoHH <CaoHH@894f9759-fa2a-45cf-9159-0a04a166245e>
2013-11-18 08:23:40 +0000

    定义bubble协议远程预览接口
    git-svn-id: svn://192.168.2.188/qtcms@171 894f9759-fa2a-45cf-9159-0a04a166245e

---
+++ include/IDeviceConnection.h
@@ -0,0 +1,28 @@
+#ifndef __ILOGINDEVICE_HEAD_FILE_ASDNVG8Y9ASDF__
+#define __ILOGINDEVICE_HEAD_FILE_ASDNVG8Y9ASDF__
+#include <libpcom.h>
+#include <QtCore/QVariantMap>
+#include <QtCore/QString>
+
+interface IDeviceConnection : public IPComBase
+{
+	virtual int setDeviceHost(const QString & sAddr) = 0;
+	virtual int setDevicePorts(const QVariantMap & ports) = 0;
+	virtual int setDeviceId(const QString & sAddress) = 0;
+	virtual int setDeviceAuthorityInfomation(QString username,QString password) = 0;
+	virtual int connectToDevice() = 0;
+	virtual int disconnect() = 0;
+	virtual int getCurrentStatus() = 0;
+	virtual QString getDeviceHost() = 0;
+	virtual QString getDeviceid() = 0;
+	virtual QVariantMap getDevicePorts() = 0;
+	enum _enConnectionStatus{
+		CS_Disconnected,
+		CS_Connectting,
+		CS_Connected,
+		CS_Disconnecting
+	};
+};
+
+
+#endif
---
+++ include/IRemotePreview.h
@@ -0,0 +1,35 @@
+#ifndef __IPREVIEWCAM_HEAD_FILE_NS89VY9ASUD8__
+#define __IPREVIEWCAM_HEAD_FILE_NS89VY9ASUD8__
+#include <libpcom.h>
+#include <QtCore/QString>
+#include <QtCore/QVariantMap>
+
+interface IRemotePreview : public IPComBase
+{
+	virtual int getLiveStream(int nChannel, int nStream) = 0;
+	virtual int stopStream() = 0;
+	virtual int pauseStream(bool bPaused) = 0;
+	virtual int getStreamCount() = 0;
+	virtual int getStreamInfo(int nStreamId,QVariantMap &streamInfo) = 0;
+};
+
+/*
+Event:
+@1	name: "LiveStream"
+	parameters:
+		"channel":当前帧的通道号
+		"pts":当前帧时间戳，单位为微秒
+		"length":数据长度
+		"data":数据指针
+		"frametype":帧类型，取值'I','P','B','A'
+		"width":视频帧的宽，单位像素，如果是音频帧，不传递该参数
+		"height":视频帧的高，单位像素，如果是音频帧，不传递该参数
+		"vcodec":视频帧的编码格式，当前定义值:"H264"，如果是音频帧，不传递该参数
+		"samplerate":音频采样率，如果是视频帧，不传递该参数
+		"samplewidth":音频采样位宽，如果是视频帧，不传递该参数
+		"audiochannel":音频的采样通道数，如果是视频帧，不传递该参数
+		"acodec":音频编码格式，当前定义值："G711"，如果是视频帧，不传递该参数
+*/
+
+
+#endif
