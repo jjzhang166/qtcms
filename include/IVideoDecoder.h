04fc3d5df7185e4e54a878ee816e4e5d4325b89b
CaoHH <CaoHH@894f9759-fa2a-45cf-9159-0a04a166245e>
2013-11-21 10:15:38 +0000

    修改解码和渲染接口，适配多种解码器输出方式和多种渲染器输入方式
    git-svn-id: svn://192.168.2.188/qtcms@184 894f9759-fa2a-45cf-9159-0a04a166245e

--- include/IVideoDecoder.h
+++ include/IVideoDecoder.h
@@ -14,13 +14,27 @@ interface IVideoDecoder : public IPComBase
 Event:
 @1	name: "DecodedFrame"
 	parameters:
-		"Ydata":Y数据
-		"Udata":U数据
-		"Vdata":V数据
+		”flags“:可用参数标志，按位计算，当对应位置位时，表示该参数可用，否则参数不可用
+				0x00000001			DF_MASK_DATA
+				0x00000002			DF_MASK_YDATA
+				0x00000004			DF_MASK_UDATA
+				0x00000008			DF_MASK_VDATA
+				0x00000010			DF_MASK_WIDTH
+				0x00000020			DF_MASK_HEIGHT
+				0x00000040			DF_MASK_YSTRIDE
+				0x00000080			DF_MASK_UVSTRIDE
+				0x00000100			DF_MASK_LINESTRIDE
+				0x00000200			DF_MASK_PIXELFOMAT
+		"data":数据地址
+		"Ydata":视频Y数据地址
+		"Udata":视频U数据地址
+		“Vdata":视频V数据地址
 		"width":视频帧宽度，单位像素
 		"height":视频帧高度，单位像素
 		"YStride":Y数据行宽，单位字节
 		"UVStride":UV数据行宽，单位字节
+		"lineStride":行宽，当像素格式为RGB或者YUV打包格式时使用
+		"pixelFormat":像素格式，YUV420_P-"YV12"
 */
 
 #endif
--- include/IVideoRender.h
+++ include/IVideoRender.h
@@ -8,11 +8,12 @@ interface IVideoRender : public IPComBase
 	virtual int init(int nWidth,int nHeight) = 0;
 	virtual int deinit() = 0;
 	virtual int setRenderWnd(QWidget * wnd) = 0;
-	virtual int render(char *pYData,char *pUData,char *pVData,int nWidth,int nHeight,int nYStride,int nUVStride) = 0;
+	virtual int render(char *data,char *pYData,char *pUData,char *pVData,int nWidth,int nHeight,int nYStride,int nUVStride,int lineStride,const QString & pixelFormat,int flags) = 0;
 	virtual int enable(bool bEnable) = 0;
 	virtual int enableStretch(bool bEnable) = 0;
 	virtual bool isRenderEnable() = 0;
 	virtual bool isStretchEnable() = 0;
+	virtual bool isPixelFormatAvalible(const QString &sFormat) = 0;
 };
 
 #endif
