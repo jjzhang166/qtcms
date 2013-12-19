8450fc9e4310b3813cfd987decf85bd432096a96
HuangYY <HuangYY@894f9759-fa2a-45cf-9159-0a04a166245e>
2013-12-19 01:06:23 +0000

    12.19
    git-svn-id: svn://192.168.2.188/qtcms@397 894f9759-fa2a-45cf-9159-0a04a166245e

--- web/js/ocxfunction.js
+++ web/js/ocxfunction.js
@@ -208,6 +208,7 @@ var oSelected = [],
 	//区域分组,属性菜单输出.
 	function areaList2Ui(){ //区域菜单输出
 		$('div.dev_list span.area').not('#area_0').parent('li').remove();
+		$('div.dev_list span.device').parent('li').remove();
 		//$('ul.filetree').treeview();
 		var areaListArrar=[];
 		var pidList=[];
