8f33aab1e35e87efda203d97e8e096a7a120c590
FengMC <FengMC@894f9759-fa2a-45cf-9159-0a04a166245e>
2014-01-06 12:03:50 +0000

    local player 组件同步回放接口定义
    git-svn-id: svn://192.168.2.188/qtcms@566 894f9759-fa2a-45cf-9159-0a04a166245e

---
+++ include/ILocalPlayer.h
@@ -0,0 +1,92 @@
+#ifndef __ILOCALPLAYER_HEAD_FILE_SADVN98ASD7YASGV__
+#define __ILOCALPLAYER_HEAD_FILE_SADVN98ASD7YASGV__
+#include <libpcom.h>
+#include <QtGui/QWidget>
+#include <QtCore/QDateTime>
+#include <QStringList>
+interface ILocalPlayer:IPComBase
+{
+	//搜索本地限定时间内可播放的文件
+	//输入参数：
+	//start：限定的开始时间(时间格式："yyyy-MM-dd hh:mm:ss")
+	//end：限定的结束时间(时间格式："yyyy-MM-dd hh:mm:ss")
+	//filelist:返回搜索到的文件list
+	//返回值：
+	//0：成功
+	//1：失败
+	virtual int SearchLocalPlayFileList(const QDateTime &start,const QDateTime &end,QStringList &filelist)=0;
+	//添加播放文件到同步组中，并指定文件播放的窗口ID
+	//输入参数：
+	//filename:文件名，包括完整的文章路径，如：F:\project\date\devname\chl2\filename.cvi
+	//wnd:播放窗口的id
+	//返回值：
+	//0：添加成功
+	//1：添加失败，通道组已经满
+	//2：添加失败，窗口已经被占用
+	//3：添加失败，（失败原因未定义）
+	virtual int AddFileIntoPlayGroup(QString const filename,QWidget *wnd)=0;
+	//通过文件文件名从播放组中去除文件
+	//输入参数：
+	//filename：文件名称，包括完整的文章路径，如：F:\project\date\devname\chl2\filename.cvi
+	//返回值：
+	//0：去除成功
+	//1：去除失败
+	virtual int RemoveFileFromPlayGroupByFilename(QString const filename)=0;
+	//通过窗口id从播放组中去除文件
+	//输入参数：
+	//Wnd：窗口的id
+	//返回值：
+	//0：去除成功
+	//1：去除失败
+	virtual int RemoveFileFromPlayGroupByWnd(QWidget *Wnd)=0;
+	//设置同步组数量
+	//输入参数：
+	//num:设置同步组的数量值
+	//返回值：
+	//0：设置成功
+	//1：设置失败
+	virtual int SetSynGroupNum(int num)=0;
+	//组播放
+	//输入参数:
+	//start:开始播放时间(时间格式："yyyy-MM-dd hh:mm:ss")
+	//end：结束播放时间(时间格式："yyyy-MM-dd hh:mm:ss")
+	//返回值：
+	//0：调用成功
+	//1：调用失败
+	virtual int GroupPlay(const QDateTime & start,const QDateTime &end)=0;
+	//暂停播放
+	//返回值：
+	//0：调用成功
+	//1：调用失败
+	virtual int GroupPause()=0;
+	//继续播放
+	//返回值：
+	//0：调用成功
+	//调用失败
+	virtual int GroupContinue()=0;
+	//停止播放
+	//返回值：
+	//0：调用成功
+	//1：调用失败
+	virtual int GroupStop()=0;
+	//快进
+	//输入参数：
+	//speed：正常播放速度的倍数（可选值：2，4，8）
+	//返回值：
+	//0：调用成功
+	//1：调用失败
+	virtual int GroupSpeedFast(int speed)=0;
+	//慢放
+	//输入参数：
+	//speed：正常播放速度的1/N倍（可选值：2，4，8）
+	//返回值：
+	//0：调用成功
+	//1：调用失败
+	virtual int GroupSpeedSlow(int speed)=0;
+	//恢复正常的播放速度
+	//返回值：
+	//0：调用成功
+	//1：调用失败
+	virtual int GroupSpeedNormal() = 0;
+};
+#endif
