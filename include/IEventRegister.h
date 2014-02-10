7420122d78b55098a039ed522f74772a7873be6c
FengMC <FengMC@894f9759-fa2a-45cf-9159-0a04a166245e>
2014-02-10 01:53:32 +0000

    添加ieventregister接口的备注
    git-svn-id: svn://192.168.2.188/qtcms@843 894f9759-fa2a-45cf-9159-0a04a166245e

--- include/IEventRegister.h
+++ include/IEventRegister.h
@@ -7,8 +7,24 @@
 
 interface IEventRegister : public IPComBase
 {
+	//返回事件列表
 	virtual QStringList eventList() = 0;
+	//查询事件参数
+	//eventName：请求返回事件参数的名称
+	//eventParams：返回事件参数的列表
+	//返回值：
+	//OK：成功
+	//E_EVENT_NOT_SUPPORT：无此事件
+	//E_INVALID_PARAM：系统错误
 	virtual int queryEvent(QString eventName,QStringList &eventParams) = 0;
+	//注册事件
+	//eventName：注册的事件名称
+	//proc：注册事件的回调函数
+	//pUser：注册事件的父指针
+	//返回值：
+	//OK：成功
+	//E_EVENT_NOT_SUPPORT：无此事件
+	//E_INVALID_PARAM：系统错误
 	virtual int registerEvent(QString eventName,int (__cdecl *proc)(QString,QVariantMap,void *),void *pUser) = 0;
 
 	enum _enErrorCode{
