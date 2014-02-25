5589587ce31754220bbd72bf75ebd6b9978d0b8f
FengMC <FengMC@894f9759-fa2a-45cf-9159-0a04a166245e>
2014-02-25 10:04:13 +0000

    git-svn-id: svn://192.168.2.188/qtcms@976 894f9759-fa2a-45cf-9159-0a04a166245e

---
+++ src/plugins/DeviceSearchPlugin/DeviceSearchWindows_global.h
@@ -0,0 +1,17 @@
+#ifndef DEVICESEARCHWINDOWS_GLOBAL_H_
+#define DEVICESEARCHWINDOWS_GLOBAL_H_
+
+#include <QtCore/qglobal.h>
+
+
+
+#pragma pack()
+
+typedef struct _tagDevNetworkInfo{
+	QString IpPart1;
+	QString IpPart2;
+	QString IpPart3;
+	QString IpPart4;
+}DevNetworkInfo;
+
+#endif // DEVICESEARCHWINDOWS_GLOBAL_H_
