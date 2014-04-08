58002a748e4b167769bbce2db9e93da819c64ec5
CaoHH <CaoHH@894f9759-fa2a-45cf-9159-0a04a166245e>
2014-04-08 04:12:11 +0000

    声音调试
    git-svn-id: svn://192.168.2.188/qtcms@1178 894f9759-fa2a-45cf-9159-0a04a166245e

--- web/js/ocxfunction.js
+++ web/js/ocxfunction.js
@@ -1,6 +1,7 @@
 
 var oCommonLibrary,
-	bool = 0; //本地远程回放控制  0为远程 1为本地
+	bool = 0, //本地远程回放控制  0为远程 1为本地
+	recTotal = 0;  //文件检索总数。
 	$(function(){ 
 		//return false;
 		oCommonLibrary = document.getElementById('commonLibrary');
@@ -428,7 +429,6 @@ function setDevData2ocx(){
 			try{PBrecFileTableInit();}catch(e){}
 		}
 		$('#channelvideo div.video').remove();
-		$('#fileRec').show();
 		  //cgi 请求数据
 		/*var channels = 0;   
 		$('#channelvideo input:checkbox').each(function(index){ 
