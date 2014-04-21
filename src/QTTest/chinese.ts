17b41bd2cfd795c914ba205f28f02533f4eb5b5f
simon <mingchuangfeng@163.com>
2014-04-21 16:14:22 +0800

    add mul language ts

---
+++ src/QTTest/chinese.ts
@@ -0,0 +1,23 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>QSubView</name>
+    <message>
+        <source>Close Preview</source>
+        <translation>关闭预览</translation>
+    </message>
+    <message>
+        <source>Switch Stream</source>
+        <translation>切换码流</translation>
+    </message>
+    <message>
+        <source>Switch to SubStream</source>
+        <translation>子码流</translation>
+    </message>
+    <message>
+        <source>Switch to MainStream</source>
+        <translation>主码流</translation>
+    </message>
+</context>
+</TS>
---
+++ src/QTTest/english.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>QSubView</name>
+    <message>
+        <source>Close Preview</source>
+        <translation>Close Preview</translation>
+    </message>
+    <message>
+        <source>Switch Stream</source>
+        <translation>Switch Stream</translation>
+    </message>
+    <message>
+        <source>Switch to SubStream</source>
+        <translation>Switch to SubStream</translation>
+    </message>
+    <message>
+        <source>Switch to MainStream</source>
+        <translation>Switch to MainStream</translation>
+    </message>
+</context>
+</TS>
