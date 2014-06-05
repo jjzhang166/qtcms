ad53405fddad357da86c1a9556a1f4b22488f70c
simon <mingchuangfeng@163.com>
2014-06-05 11:55:23 +0800

    qdebug message save to local file:debug.log

--- src/QTTest/main.cpp
+++ src/QTTest/main.cpp
@@ -3,10 +3,45 @@
 #include "qjawebview.h"
 #include "libpcom.h"
 #include <QEventLoop>
+#include <QtDebug>
+#include <QFile>
+#include <QDateTime>
+#include <QTextStream>
+void customMessageHandler(QtMsgType type, const char *msg){
+	QString txt;  
+	switch (type) {  
+	case QtDebugMsg:  
+		txt = QString("Debug: %1").arg(msg);
+		txt.append("-----");
+		txt.append(QDateTime::currentDateTime().toString("dd.MM.yyyy-hh:mm:ss.zzz"));
+		break;  
+
+	case QtWarningMsg:  
+		txt = QString("Warning: %1").arg(msg);  
+		break;  
+	case QtCriticalMsg:  
+		txt = QString("Critical: %1").arg(msg);  
+		break;  
+	case QtFatalMsg:  
+		txt = QString("Fatal: %1").arg(msg);  
+		abort();  
+	}  
+
+	QFile outFile("debug.log");  
+	outFile.open(QIODevice::WriteOnly | QIODevice::Append);  
+	QTextStream ts(&outFile);  
+	ts << txt << endl; 
+}
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
+	//debug log
+	qInstallMsgHandler(customMessageHandler);
+	QFile outFile("debug.log");  
+	outFile.open(QIODevice::WriteOnly | QIODevice::Append);  
+	QTextStream ts(&outFile);  
+	QString txt="\r\n\r\n=====start=====\r\n\r\n";
+	ts << txt << endl; 
 	// Get Application Path
 	QString sTemp = QCoreApplication::applicationDirPath();
 	// extern libraries path
@@ -16,6 +51,6 @@ int main(int argc, char *argv[])
 	QEventLoop eventloop;
 	QTimer::singleShot(1000,&eventloop,SLOT(quit()));
 	eventloop.exec();
-    view.showMaximized();
+    view.showMaximized(); 
     return a.exec();
 }
