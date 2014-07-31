5f86013f7368b0f0d8c1851759b37a9513a6302f
simon <mingchuangfeng@163.com>
2014-07-31 21:37:00 +0800

    rename version name

--- src/QTTest/qjawebview.cpp
+++ src/QTTest/qjawebview.cpp
@@ -32,7 +32,7 @@ QJaWebView::QJaWebView(QWidget *parent) :
 	setContextMenuPolicy(Qt::NoContextMenu);
 #ifdef __USE_WEB_DEBUGER__
 	QWebSettings::globalSettings()->setAttribute(QWebSettings::DeveloperExtrasEnabled,true);
-	Dumper::setVersionInfo("cms_1.1.12_07_29_20", strlen("cms_1.1.12_07_29_20"));
+	Dumper::setVersionInfo("cms_1.1.12_07_31_21", strlen("cms_1.1.12_07_31_21"));
 	m_pdup = new Dumper();
 #endif
 
