16ce9d8b1a37ee941e5d6099368579c12350d2d6
ZhaoCS <327217148@qq.com>
2014-08-09 15:29:09 +0800

    correct index.html blur/focus bug
    Conflicts:
    	web/js/index.js

--- web/js/index.js
+++ web/js/index.js
@@ -509,11 +509,12 @@ var oPreView,oDiv,
 		//分组列表;
 		groupList2Ui();
 
-		$('span.channel').removeClass('channel_1');
+		//$('span.channel').removeClass('channel_1');
 
 		for(var i=0;i<64;i++){
 			var oWinInfo = oPreView.GetWindowInfo(i);
-			if(oWinInfo.chlId!=-1 && oWinInfo.currentState == 0){
+			//console.log(oWinInfo);
+			if(oWinInfo.chlId>0 && oWinInfo.currentState == 0){
 				var chlData = $('#channel_'+oWinInfo.chlId).attr({
 					wind:i,
 					state:oWinInfo.currentState
