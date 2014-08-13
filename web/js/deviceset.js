b70c124a666c4fbe82b61e49988698b05c93950f
ZhaoCS <327217148@qq.com>
2014-08-13 16:22:10 +0800

    correct HYH code/playback add ThrowException
    Conflicts:
    	web/js/devicesetting/ipc.class.js
    	web/js/playback.js

--- web/js/deviceset.js
+++ web/js/deviceset.js
@@ -150,11 +150,11 @@ var oSearchOcx,
 						}else{   /*    if(oDevData.vendor == 'DVR' || oDevData.vendor == 'NVR')//如果选中设备为DVR或NVR*/
 							$('ul.dvr_list0 li').eq(0).addClass('ope_listAct').siblings('li').removeClass('ope_listAct').parent('ul').show();
 							$('.dvr_list').eq(0).show();
-							 /*emptyDevSetMenu();
+							//emptyDevSetMenu();
                          
 							dvr(_url,oDevData.username,oDevData.password,oDevData.channel_count);
-							dvr_devinfo_load_content();*/
-							console.log('------------new DVR()--------------');
+							dvr_devinfo_load_content();
+							/*console.log('------------new DVR()--------------');
 							nowDev = new DVR(oDevData.username,oDevData.password,oDevData.address,oDevData.port,oDevData.dev_id,oDevData.vendor);
 							
 							$('#set_content ul.dvr_list0 li').click(function(){//stop(true,true)解决不断用鼠标点击产生的积累
@@ -605,8 +605,7 @@ var oSearchOcx,
 
 		//$('#set_content div.switch input[class]').val('').prop('checked',false);
 
-		$('#set_content div.switch:visible').find('input[data-UI]:text,input[data-UI]:password,input[data-UI][type="select"]').val('').attr('data','')
-									  		  .end().find(':checkbox,:radio').prop('checked',false); //清除
+		$('#set_content div.switch:visible').find('input[data-UI]:text,input[data-UI]:password,input[data-UI][type="select"]').val('').attr('data','').css('border','0').end().find(':checkbox,:radio').prop('checked',false); //清除
 		$('#ajaxHint').html('').stop(true,true).hide();
 		//$('#ajaxHint').stop(true,true).css('top',targetMenu.height() + 46).html(lang.loading).show();
 	}
@@ -960,6 +959,7 @@ var userLev = [lang.Super_Admin,lang.Admin,lang.User,lang.Tourists];
 			$('<tr id="esee_'+id+'" class="'+data.SearchVendor_ID+'"><td><input type="checkbox" />'+data.SearchVendor_ID+'</td><td>'+data.SearchSeeId_ID+'</td><td>'+data.SearchIP_ID+'</td><td>'+data.SearchChannelCount_ID+'</td></tr>').appendTo($('#SerachDevList tbody')).data('data',data);
 			//initDevIntoAreaXml($('#SerachDevList tbody input:checkbox'),$('#adddevicedouble_ID'));
 		}
+
 
 		var warp = $('#SerachDevList');
 
