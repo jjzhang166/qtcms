a9f736ed581259956c339f0609997741052456f6
simon <mingchuangfeng@163.com>
2014-08-28 17:25:14 +0800

    modify database

--- doc/database/recordEx.sql
+++ doc/database/recordEx.sql
@@ -1,19 +1,9 @@
-create table local_record
-(
-id integer primary key autoincrement,
-dev_chl integer,
-win_id integer,
-record_type integer,
-start_time integer,
-end_time integer,
-file_size integer,
-path char(64)
-);
 create table search_record
 {
 id integer primary key autoincrement,
 win_id integer,
 record_type integer,
 start_time char(32),
-end_time char(32)
+end_time char(32),
+path char(64)
 };
