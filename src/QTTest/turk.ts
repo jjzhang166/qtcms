aa14c1017619e511a0e1e8044b046439f435bd6c
CaoHH <caostorm@163.com>
2014-09-05 09:35:47 +0800

    Add turk language file of preview plugin.

---
+++ src/QTTest/turk.ts
@@ -0,0 +1,58 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>QSubView</name>
+    <message>
+        <source>Close Preview</source>
+        <translation>Kapat Önizleme</translation>
+    </message>
+    <message>
+        <source>Switch Stream</source>
+        <translation>Akış anahtarı</translation>
+    </message>
+    <message>
+        <source>Switch to SubStream</source>
+        <translation>Alt akımını geçin</translation>
+    </message>
+    <message>
+        <source>Switch to MainStream</source>
+        <translation>Ana akımına anahtarı</translation>
+    </message>
+        <message>
+        <source>Start Record</source>
+        <translation >kayıt başlatın</translation>
+    </message>
+    <message>
+        <source>Stop Record</source>
+        <translation >kaydı durdurma</translation>
+    </message>
+</context>
+<context>
+    <name>qsubviewEx</name>
+    <message>
+        <source>Close Preview</source>
+        <translation >Kapat Önizleme</translation>
+    </message>
+    <message>
+        <source>Switch Stream</source>
+        <translation >Akış anahtarı</translation>
+    </message>
+    <message>
+        <source>Switch to SubStream</source>
+        <translation >Alt akımını geçin</translation>
+    </message>
+    <message>
+        <source>Switch to MainStream</source>
+        <translation >Ana akımına anahtarı</translation>
+    </message>
+    <message>
+        <source>Start Record</source>
+        <translation >kayıt başlatın</translation>
+    </message>
+    <message>
+        <source>Stop Record</source>
+        <translation >kaydı durdurma</translation>
+    </message>
+</context>
+</TS>
