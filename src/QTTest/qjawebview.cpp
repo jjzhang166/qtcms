3eaaa4ca37b7b2ad4c15fed85cd742de4bc141b1
simon <mingchuangfeng@163.com>
2014-09-20 10:46:15 +0800

    and tag

--- src/QTTest/qjawebview.cpp
+++ src/QTTest/qjawebview.cpp
@@ -33,7 +33,7 @@ QJaWebView::QJaWebView(QWidget *parent) :
 #ifdef __USE_WEB_DEBUGER__DUMP__
 	QWebSettings::globalSettings()->setAttribute(QWebSettings::DeveloperExtrasEnabled,true);
 #ifdef WIN32
-	Dumper::setVersionInfo("cms_1.1.12_08_21_17", strlen("cms_1.1.12_08_21_17"));
+	Dumper::setVersionInfo("cms_1.1.13_09_20_10", strlen("cms_1.1.13_09_20_10"));
 	m_pdup = new Dumper();
 #endif
 #endif
