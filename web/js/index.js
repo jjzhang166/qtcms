5f08f70c00dc9dcb48d998564899cddd8a28f893
yuyihuang <yanhuihuang@juanvision.com>
2014-09-22 13:57:43 +0800

    fix some bug

--- web/js/index.js
+++ web/js/index.js
@@ -208,7 +208,7 @@ var oPreView,oDiv,
 		//window.status = '<pageaction SrcUrl="/skins/default/index.html" SrcAct="index" DstUrl="/skins/default/log.html" DstAct="reload"></pageaction>';
 	})///
 	
-	$(window).resize(ViewMax);
+	$(window).resize(viewFullScreen);
 	
 	function ViewMax(){
 		var W = $(window).width();
