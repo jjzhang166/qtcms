c4eb0efbabc8d09dc46cd411f84b4de7a5cd420b
simon <mingchuangfeng@163.com>
2015-02-07 11:16:22 +0800

    onvif setting

--- src/plugins/onvifSetting/onvifSettingObject.cpp
+++ src/plugins/onvifSetting/onvifSettingObject.cpp
@@ -1,41 +1,96 @@
 #include "onvifSettingObject.h"
-
-
-onvifSettingObject::onvifSettingObject():QWebPluginFWBase(this)
+#include <guid.h>
+#include <QDebug>
+onvifSettingObject::onvifSettingObject():QWebPluginFWBase(this),
+	m_pIOnvifReoteInfo(NULL)
 {
+	pcomCreateInstance(CLSID_OnvifNetwork,NULL,IID_IOnvifRemoteInfo,(void**)&m_pIOnvifReoteInfo);
 }
 
 
 onvifSettingObject::~onvifSettingObject()
 {
+	if (m_pIOnvifReoteInfo!=NULL)
+	{
+		m_pIOnvifReoteInfo->Release();
+		m_pIOnvifReoteInfo=NULL;
+	}
 }
 
-QVariantMap onvifSettingObject::getOnvifDeviceNetwordInfo()
+QVariantMap onvifSettingObject::getOnvifDeviceNetworkInfo()
 {
 	QVariantMap tInfo;
-	tInfo.insert("sMac","sMacValue");
-	tInfo.insert("sGateway","sGatewayValue");
-	tInfo.insert("sMask","sMaskValue");
-	tInfo.insert("sDns","sDnsValue");
+	QString sMac;
+	QString sGateway;
+	QString sMask;
+	QString sDns;
+	if (NULL!=m_pIOnvifReoteInfo)
+	{
+		if (m_pIOnvifReoteInfo->getOnvifDeviceNetworkInfo(sMac,sGateway,sMask,sDns))
+		{
+			tInfo.insert("sMac",sMac);
+			tInfo.insert("sGateway",sGateway);
+			tInfo.insert("sMask",sMask);
+			tInfo.insert("sDns",sDns);
+		}else{
+			qDebug()<<__FUNCTION__<<__LINE__<<"get networkInfo fail as getOnvifDeviceNetworkInfo fail";
+		}
+	}
 	return tInfo;
 }
 
-bool onvifSettingObject::setOnvifDeviceNetWordInfo(QString sSetIp,QString sSetMac,QString sSetGateway,QString sSetMask,QString sSetDns )
+bool onvifSettingObject::setOnvifDeviceNetWorkInfo(QString sSetIp,QString sSetMac,QString sSetGateway,QString sSetMask,QString sSetDns )
 {
+	if (NULL!=m_pIOnvifReoteInfo)
+	{
+		if (m_pIOnvifReoteInfo->setOnvifDeviceNetWorkInfo(sSetIp,sSetMac,sSetGateway,sSetMask,sSetDns))
+		{
+			return true;
+		}else{
+			qDebug()<<__FUNCTION__<<__LINE__<<"set fail as the under Function return fail";
+		}
+	}else{
+		qDebug()<<__FUNCTION__<<__LINE__<<"set fail as m_pIOnvifReoteInfo is null";
+	}
 	return false;
 }
 
 QString onvifSettingObject::getOnvifDeviceEncoderInfo()
 {
-	return false;
+	QString sEncoderInfo;
+	if (NULL!=m_pIOnvifReoteInfo)
+	{
+		sEncoderInfo=m_pIOnvifReoteInfo->getOnvifDeviceEncoderInfo();
+	}else{
+		qDebug()<<__FUNCTION__<<__LINE__<<"get DeviceEncoderInfo fail as m_pIOnvifReoteInfo is null";
+	}
+	return sEncoderInfo;
 }
 
 bool onvifSettingObject::setOnvifDeviceEncoderInfo( int nIndex,int nWidth,int nHeight,QString sEnc_fps,QString sEnc_bps,QString sCodeFormat,QString sEncInterval,QString sEncProfile )
 {
-	return "test";
+	if (NULL!=m_pIOnvifReoteInfo)
+	{
+		if (m_pIOnvifReoteInfo->setOnvifDeviceEncoderInfo(nIndex,nWidth,nHeight,sEnc_fps,sEnc_bps,sCodeFormat,sEncInterval,sEncProfile))
+		{
+			return true;
+		}else{
+			qDebug()<<__FUNCTION__<<__LINE__<<"set EncoderInfo fail as under Function return fail";
+		}
+	}else{
+		qDebug()<<__FUNCTION__<<__LINE__<<"set EncoderInfo fail as m_pIOnvifReoteInfo is null";
+	}
+	return false;
 }
 
 bool onvifSettingObject::setOnvifDeviceParam( QString sIp,QString sPort,QString sUserName,QString sPassword )
 {
+	if (NULL!=m_pIOnvifReoteInfo)
+	{
+		m_pIOnvifReoteInfo->setOnvifDeviceInfo(sIp,sPort,sUserName,sPassword);
+		return true;
+	}else{
+		qDebug()<<__FUNCTION__<<__LINE__<<"set device param fail as m_pIOnvifReoteInfo is null";
+	}
 	return false;
 }
--- src/plugins/onvifSetting/onvifSettingObject.h
+++ src/plugins/onvifSetting/onvifSettingObject.h
@@ -2,6 +2,7 @@
 
 #include <qwfw.h>
 #include <QVariantMap>
+#include <IOnvifRemoteInfo.h>
 class onvifSettingObject:public QWidget,
 	public QWebPluginFWBase
 {
@@ -11,9 +12,11 @@ public:
 	~onvifSettingObject();
 public slots:
 	bool setOnvifDeviceParam(QString sIp,QString sPort,QString sUserName,QString sPassword);
-	QVariantMap getOnvifDeviceNetwordInfo();
-	bool setOnvifDeviceNetWordInfo(QString sSetIp,QString sSetMac,QString sSetGateway,QString sSetMask,QString sSetDns);
+	QVariantMap getOnvifDeviceNetworkInfo();
+	bool setOnvifDeviceNetWorkInfo(QString sSetIp,QString sSetMac,QString sSetGateway,QString sSetMask,QString sSetDns);
 	QString getOnvifDeviceEncoderInfo();
 	bool setOnvifDeviceEncoderInfo(int nIndex,int nWidth,int nHeight,QString sEnc_fps,QString sEnc_bps,QString sCodeFormat,QString sEncInterval,QString sEncProfile);
+private:
+	IOnvifRemoteInfo *m_pIOnvifReoteInfo;
 };
 
